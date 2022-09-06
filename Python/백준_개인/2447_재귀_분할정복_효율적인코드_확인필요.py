d54d5b40a73908e9a57900d2866068e410e2cb6e
dortkthf <dortkthf@gmail.com>
2022-09-06 21:45:28 +0900

    PRAC업데이트

--- Python/백준_개인/2447_재귀_분할정복_효율적인코드_확인필요.py
+++ Python/백준_개인/2447_재귀_분할정복_효율적인코드_확인필요.py
@@ -4,10 +4,8 @@ cnt = 0
 def stars(n):
     global cnt
     if n>3 :
-        n/=3
         cnt+=1
-        stars(n)
-        n*=3
+        stars(n//3)
         cnt-=1
     a = int(n/3)   
     for x in range(3**cnt):
@@ -20,4 +18,45 @@ stars(n)
 for s in star:
     print(*s,sep='')
 
+# n = int(input())
 
+# llist = []
+# def star(l):
+#     if l == 3:
+#         return ['***','* *','***']
+
+#     arr = star(l//3)
+#     llist.append(l)
+#     stars = []
+
+#     for i in arr:
+#         stars.append(i*3)
+
+#     for i in arr:
+#         stars.append(i+' '*len(arr)+i)
+
+#     for i in arr:
+#         stars.append(i*3)
+
+#     return stars
+# for i in star(n):
+#     print(*i)
+# print(llist)
+
+n = int(input())
+stars = ['***','* *','***']
+def makestars(n) :
+    if n == 3:
+        return stars
+    starss = makestars(n//3)
+    l = []
+    for s in starss:
+        l.append(s*3)
+    for s in starss:
+        l.append(s+' '*len(starss)+s)
+    for s in starss:
+        l.append(s*3)
+    return l
+
+for i in makestars(n):
+    print(*i,sep='')
