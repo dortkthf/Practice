0d730316aa2b683ff5be145c027585ab08f0b805
dortkthf <dortkthf@gmail.com>
2022-09-26 17:52:25 +0900

    django prac

---
+++ Django/Prac_0926/fstapp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+def oddEven(request, number):
+    context = {
+        "number": number,
+    }
+    return render(request, "oddEven.html", context)
+
+
+def calculator(request, number1, number2):
+    context = {
+        "number1": number1,
+        "number2": number2,
+    }
+    return render(request, "calculator.html", context)
+
+
+def pastLife(request):
+
+    return render(request, "pastLife.html")
+
+
+def pastLife_result(request):
+    name = request.GET.get("name")
+    pastL = ["사자", "호랑이", "늑대"]
+    past = random.choice(pastL)
+    if past == "호랑이":
+        img = "https://sojoong.joins.com/wp-content/uploads/sites/4/2021/07/KakaoTalk_20210708_191738164.jpg"
+    elif past == "사자":
+        img = "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202001/01/c9fc6597-f43b-40f8-a1ae-ea3f0a426210.jpg"
+    else:
+        img = "https://img4.yna.co.kr/etc/inner/KR/2020/02/09/AKR20200209003700091_01_i_P4.jpg"
+    context = {"name": name, "img": img, "past": past}
+    return render(request, "pastLife_result.html", context)
+
+
+def kinput(request):
+
+    return render(request, "kinput.html")
+
+
+def kresult(request):
+    pnum = request.GET.get("pnum")
+    pn = list(range(int(pnum)))
+    pword = request.GET.get("pword")
+    pw = list(range(int(pword)))
+    context = {"pnum": pnum, "pword": pword, "pn": pn, "pw": pw}
+    return render(request, "kresult.html", context)
