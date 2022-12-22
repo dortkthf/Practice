872da029b5d6d55ffc8c46a2e945d02a440fa681
dortkthf <dortkthf@gmail.com>
2022-12-22 23:53:01 +0900

    1929python solving

--- Python/백준_개인/11653.py
+++ Python/백준_개인/11653.py
@@ -1,12 +1,12 @@
 n = int(input())
-if n == 1:
-    pass
-else:
-    while n != 1:
-        for i in range(2, n + 1):
-            if n % i == 0:
-                print(i)
-                n = n // i
-                break
-        if n == 1:
+# if n == 1:
+#     pass
+# else:
+while n != 1:
+    for i in range(2, n + 1):
+        if n % i == 0:
+            print(i)
+            n = n // i
             break
+    if n == 1:
+        break
