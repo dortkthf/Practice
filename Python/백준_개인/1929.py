60a51c0de8dad99e17536110aeb3c5fc7f434ef1
dortkthf <dortkthf@gmail.com>
2022-12-23 00:32:03 +0900

    solve 1929.py

--- Python/백준_개인/1929.py
+++ Python/백준_개인/1929.py
@@ -1,12 +1,10 @@
-from sys import stdin
-
 m, n = map(int, input().split())
 for i in range(m, n + 1):
     cnt = 0
-    for j in range(1, i + 1):
+    for j in range(2, int(i**0.5) + 1):
         if i % j == 0:
             cnt += 1
-        if cnt > 2:
+        if cnt != 0:
             break
-        if cnt == 2:
-            print(i)
+    if cnt == 0 and i != 1:
+        print(i)
