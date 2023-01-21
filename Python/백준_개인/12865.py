a5b940c1cd1cf979a08d712c9b7cfe1b6f362d6b
dortkthf <dortkthf@gmail.com>
2023-01-21 23:45:19 +0900

    solving

---
+++ Python/백준_개인/12865.py
@@ -0,0 +1,2 @@
+n,k = map(int,input().split())
+things = [list(map(int,input().split())) for i in range(n)]
--- Python/백준_개인/9251.py
+++ Python/백준_개인/9251.py
@@ -1,26 +1,13 @@
 a = input()
 b = input()
 
-alen = len(a)
-blen = len(b)
-if alen>blen:
-    big = a
-    small = b
-else:
-    big = b
-    small = a
+dp = [list(0 for i in range(len(a)+1)) for i in range(len(b)+1)]
 
-dp = []
-for i in range(len(big)):
-    for j in range(len(small)):
-        if small[j] == big[i] and j not in dp:
-            dp.append(j)
+for i in range(len(b)):
+    for j in range(len(a)):
+        if b[i] == a[j]:
+            dp[i+1][j+1] = dp[i][j] +1
+        else:
+            dp[i+1][j+1] = max(dp[i][j+1],dp[i+1][j])
 
-res = [1 for i in range(len(dp))]
-
-for i in range(1,len(dp)):
-    for j in range(i):
-        if dp[i]>dp[j]:
-            res[i] = max(res[j]+1,res[i])
-
-print(max(res))
+print(dp[len(b)][len(a)])
