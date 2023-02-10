9dbc129c540342b1528d0296292b01eef82f1420
dortkthf <dortkthf@gmail.com>
2023-02-10 22:46:03 +0900

    solving

--- Python/백준_개인/6549.py
+++ Python/백준_개인/6549.py
@@ -1,133 +1,68 @@
-import copy, sys
+import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**5)
 
-def init(a,tree,node,start,end):
+def init(node,start,end):
+    global tree
     if start == end:
         tree[node] = start
-    else:
-        mid = (start+end)//2
-        init(a,tree, node*2, start, mid)
-        init(a, tree, node*2+1, mid+1,end)
-        if nums[tree[node*2]] > nums[tree[node*2+1]]:
-            tree[node] = tree[node*2+1]
-        else:
-            tree[node] = tree[node*2]
-
-def query(tree,node,start,end,left,right):
-    if left > end or right < start:
-        return -1
-    if left <= start and end <= right:
         return tree[node]
-    l = query(tree,node*2, start, (start+end)//2, left, right)
-    r = query(tree, node*2+1, (start+end)//2+1, end, left, right)
-    if l == -1:
-        return r
-    elif r == -1:
-        return l
+    mid = (start+end)//2
+    a = init(2*node, start,mid)
+    b = init(2*node+1,mid+1,end)
+    if nums[a] > nums[b]:
+        tree[node] = b
+        return tree[node]
     else:
-        if nums[l] > nums[r]:
-            return r
-        else:
-            return l
+        tree[node] = a
+        return tree[node]
 
-def find(start,end,n):
-    global stacks, histo, n2
-    if n == 1:
-        if stacks<histo[start]:
-            stacks = histo[start]
-        return
-    idx_min = query(tree,1,0,n2-1,start,end)
-    if stacks<histo[idx_min]*n:
-        stacks = histo[idx_min]*n
+def query(node,start,end,x,y):
+    mid = (start+end)//2
+    if start > y or end < x:
+        return inf*10
+    if x <= start and end <= y:
+        return tree[node]
 
-    if idx_min == start:   
-        find(start+1,end,end-start-1)
+    a = query(2*node,start,mid,x,y)
+    b = query(2*node+1,mid+1,end,x,y)
+    if a == inf*10:
+        return b
+    elif b == inf*10:
+        return a
+    elif nums[a] > nums[b]:
+        return b
+    else:
+        return a
+
+def find(start,end):
+    global res
+    if start == end:
+        if res < nums[start]:
+            res = nums[start]
         return
-    elif idx_min+1 == end:
-        find(start,idx_min,idx_min-start)
+    idx = query(1,0,n-1,start,end)
+    if res < nums[idx]*(end+1-start):
+        res = nums[idx]*(end+1-start)
+    if idx == start:
+        find(idx+1,end)
         return
-    find(start,idx_min,idx_min-start)
-    find(idx_min+1,end,end-idx_min-1)
+    if idx == end:
+        find(start,end-1)
+        return
+    find(start,idx-1)
+    find(idx+1,end)
     return
 
 while True:
-    stacks = 0
     nums = list(map(int,input().split()))
     n = nums[0]
-    n2 = n
-    tree = [0 for i in range(2*n+2)]
     if n == 0:
         break
-    else:
-        histo = nums[1:n+1]
-        find(0,n,n)
-        print(stacks)
-
-
-
-
-# import sys
-# sys.setrecursionlimit(10**6)
-# input = sys.stdin.readline
-
-# def init(a,tree,node,start,end):
-#     if start == end:
-#         tree[node] = start
-#     else:
-#         mid = (start+end)//2
-#         init(a,tree, node*2, start, mid)
-#         init(a, tree, node*2+1, mid+1,end)
-#         if nums[tree[node*2]] > nums[tree[node*2+1]]:
-#             tree[node] = tree[node*2+1]
-#         else:
-#             tree[node] = tree[node*2]
-
-# def query(tree,node,start,end,left,right):
-#     if left > end or right < start:
-#         return -1
-#     if left <= start and end <= right:
-#         return tree[node]
-#     l = query(tree,node*2, start, (start+end)//2, left, right)
-#     r = query(tree, node*2+1, (start+end)//2+1, end, left, right)
-#     if l == -1:
-#         return r
-#     elif r == -1:
-#         return l
-#     else:
-#         if nums[l] > nums[r]:
-#             return r
-#         else:
-#             return l
-
-# def find(s,e,n):
-#     global stack
-#     if n == 1:
-#         if stack < nums[s]:
-#             stack = nums[s]
-#         return
-#     sm_idx = query(tree,1,0,n2-1,s,e-1)
-#     if nums[sm_idx]*n > stack:
-#         stack = nums[sm_idx]*n
-#     if s == sm_idx:
-#         find(s+1,e,n-1)
-#         return
-#     elif e-1 == sm_idx:
-#         find(s,sm_idx,n-1)
-#         return
-#     find(s,sm_idx,sm_idx-s)
-#     find(sm_idx+1,e,e-sm_idx-1)
-#     return
-
-# while True:
-#     nums = list(map(int,input().split()))
-#     n = nums[0]
-#     if n == 0:
-#         break
-#     n2 = n
-#     tree = [0 for i in range(3*n)]
-
-#     stack = 0
-#     init(nums,tree,1,0,n-1)
-#     find(0,n,n)
-#     print(stack)
+    nums = nums[1:]
+    res = 0
+    inf = 101**10
+    tree = [0 for i in range(4*n)]
+    init(1,0,n-1)
+    find(0,n-1)
+    print(res)
